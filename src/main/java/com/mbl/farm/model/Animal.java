3e2873b6473d0d5456812fa0eaca3decd0f2d334
Manuel Bonat Lucia <bonat.cdz@gmail.com>
2017-09-27 12:10:06 +0200

    @ManyToOne removed from models.

--- src/main/java/com/mbl/farm/model/Animal.java
+++ src/main/java/com/mbl/farm/model/Animal.java
@@ -10,7 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -30,9 +29,6 @@ public abstract class Animal {
 	
 	private String frecuency;
 	
-	@ManyToOne
-	private User user;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "animal")
+	@OneToMany(fetch = FetchType.LAZY)
 	private List<Production> productions;
 }
--- src/main/java/com/mbl/farm/model/Production.java
+++ src/main/java/com/mbl/farm/model/Production.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -35,7 +34,4 @@ public class Production {
 	
 	@Column(name = "SELL_PRICE")
 	private Integer sellPrice;
-	
-	@ManyToOne
-	private Animal animal;
 }
